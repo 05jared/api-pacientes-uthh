c1d5c478fc990282667888d1b6380f3465cc3225
Jared Gutierrez Hernandez <guerrerojared408@gmail.com>
2026-03-13 01:34:15 -0600

    APIIIII 1

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use('/api/tipopacientes', tipopacientesRoutes);
 app.use('/api/logs', logEliminacionesRoutes);
 
 app.get('/', (req, res) => {
-  res.send('API Pacientes UTHH hola xd ');
+  res.send('API De Pacientes UTHH h');
 });
 
 app.listen(port, () => {
