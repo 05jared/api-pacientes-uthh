38c01c19246bd2b62df608e3850f16eae08c145e
Jared Gutierrez Hernandez <guerrerojared408@gmail.com>
2026-04-19 10:55:01 -0600

    CALCULO

--- src/models/medicamentos.model.js
+++ src/models/medicamentos.model.js
@@ -2,10 +2,18 @@ import db from '../config/db.js';
 
 export const getMedicamentos = async () => {
   const sql = `
-    SELECT clave, nombre, stock_inicial, consumo_por_paciente
+    SELECT
+      clave,
+      nombre,
+      stock_inicial,
+      consumo_por_paciente,
+      proveedor,
+      pacientes_por_dia,
+      reposiciones_anio
     FROM medicamentos
+    ORDER BY nombre ASC
   `;
-  
+
   const [rows] = await db.query(sql);
   return rows;
 };
